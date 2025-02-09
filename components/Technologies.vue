c00cb3bc86e2dc271c334ff4dbf3faa2a4b26c99
Hermann Witte <hermann.witte@alpha-lab.net>
2025-02-09 11:47:05 +0100

    fixed technology header & improved hover behaviour

--- components/Technologies.vue
+++ components/Technologies.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex flex-col px-4">
-    <h1 class="title text-4xl font-bold">
+  <div class="flex flex-col">
+    <h1 class="title text-3xl font-bold">
       Current technologies
     </h1>
     <p class="mt-2 font-medium text-[#848484] max-w-xl">
-      I work with a variety of modern technologies to bring my projects to life. These are the technologies I currently use the most.
+      I work with a variety of modern technologies to bring my projects to life. These are the technologies I currently use the most:
     </p>
     <div class="mt-8 grid grid-cols-2 sm:grid-cols-6 gap-x-12 gap-y-8 items-center">
       <div
@@ -24,7 +24,7 @@
             />
           </nuxt-link>
           <span
-            class="text-center text-sm font-semibold text-white opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+            class="text-center text-sm text-[#848484] font-semibold group-hover:text-white opacity-100 transition-all duration-500 group-hover:opacity-100 sm:opacity-0"
           >
             {{ tech.name }}
           </span>
