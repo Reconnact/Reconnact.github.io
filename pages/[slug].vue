1ab0e1cc2e348a08ea8ae4b60495d53bfdbb1838
hw <hw@digio.ch>
2026-03-28 01:39:25 +0100

    fixed 404 page

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -3,7 +3,10 @@
     <div class="m-auto text-3xl text-[#848484]">
       <span class="text-6xl">404</span><br><br>
       Oops... this site doesn't exist.<br><br>
-      <button class="flex mx-auto border-[#303030] border-2 p-1 px-4 rounded-lg text-2xl font-medium hover:text-white hover:border-white transition-all duration-300 ease-in-out">
+      <button
+        class="flex mx-auto border-[#303030] border-2 p-1 px-4 rounded-lg text-2xl font-medium hover:text-white hover:border-white transition-all duration-300 ease-in-out"
+        @click="$router.push('/')"
+      >
         Home
       </button>
     </div>
